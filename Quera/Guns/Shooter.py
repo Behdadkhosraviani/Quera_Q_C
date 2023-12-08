c8070bddc638a11b0ecccb00ddd37a3598080238
Behdad <kh.behdad.1999@Gmail.com>
2023-12-08 20:21:44 +0330

    Bug fixed

--- Quera/Guns/Shooter.py
+++ Quera/Guns/Shooter.py
@@ -1,6 +1,6 @@
 # first step is to determine the guns and the bullets
 Guns = {
-    "name": ["Submachine Gun", "Assault Rifle", "Pistol,Shotgun", "Sniper Rifle"],
+    "name": ["Submachine Gun", "Assault Rifle", "Pistol", "Shotgun", "Sniper Rifle"],
     "range": [100, 200, 80, 50, 1000],
     "power": [10, 20, 8, 40, 30],
     "size": [0.5, 1, 0.5, 4, 3]
@@ -29,7 +29,7 @@ class Shooter:
         if self.name == None :  # check if we already have Gun
             raise Exception("your Gun is not selected")
 
-        elif count < 0 or count == 0:  # check if count is positive
+        elif count < 0:  # check if count is positive
             raise Exception("count is out of range")
 
         elif size in Bullets["size"] and Guns["size"][self.ind] == size:
@@ -43,7 +43,10 @@ class Shooter:
         if self.ind == None or self.bullet == None :  # check if bullets or gus are chosen
             raise Exception("you don't have a bullet or a Gun")
 
-        elif target_distance > Guns["range"][self.ind] :
+        elif target_distance > Guns["range"][self.ind]:
+            return 0
+        elif self.count == 0:
+            print(0)
             return 0
         else:
             print(Guns["power"][self.ind] * Bullets["damage"][self.bullet])
@@ -54,7 +57,7 @@ class Shooter:
 
 if __name__ == "__main__":
     shooter = Shooter()
-    shooter.set_gun_by_name("Assault Rifle")
-    shooter.add_bullet_of_given_size_to_gun(1, 1)
+    shooter.set_gun_by_name("Pistol")
+    shooter.add_bullet_of_given_size_to_gun(0.5, 0)
     shooter.shoot_to_target(1, 1, 20, 5, 4)
 
